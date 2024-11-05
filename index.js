0d287e18015ee7e407c2918e9f70b08895d79694
Ralph Laurence Cruz <cruz2235726@mls.ceu.edu.ph>
2024-11-05 11:44:32 +0800

    hallo

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-// Customers
-const customerRouter = require ('./routes/customer');
-app.use('/customer', customerRouter);
+// Customers  
+const customerRouter = require('./routes/customer');  
+app.use('/customers', customerRouter);  
 
-// Payments
-const paymentRouter = require ('./routes/payment');
-app.use('/payment', paymentRouter);
+// Orders  
+const orderRouter = require('./routes/order');  
+app.use('/orders', orderRouter);  
 
-// Orders
-const orderRouter = require ('./routes/order');
-app.use('/order', orderRouter);
+// Payments  
+const paymentRouter = require('./routes/payment');
+app.use('/payments', paymentRouter);  
 
-module.exports = router;
+
